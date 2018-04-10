7644b3d24a37e47d564cf280d43303a27fc7ae89
wylie <wylie@dukeofcheese.com>
2018-04-10 13:19:57 -0400

    breaking out into components

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from '../header';
+
+import './_index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Subway Battle</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
